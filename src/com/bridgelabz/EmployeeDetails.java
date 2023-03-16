a5041e86dbb1ccbc40e94f81560b49b38b5e9700
santh98 <santhoshkumar4385@gmail.com>
2023-03-16 11:05:20 +0530

    first commit

---
+++ src/com/bridgelabz/EmployeeDetails.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class EmployeeDetails {
+
+    void add1()
+    {
+        String name, city, state, phoneNumber, department;
+        int age;
+        System.out.println(" ");
+    }
+   void add2()
+   {
+       String name, city, state, phoneNumber, department;
+       int age;
+       System.out.println(" ");
+   }
+   void add3()
+   {
+       String name, city, state, phoneNumber, department;
+       int age;
+       System.out.println(" ");
+   }
+    public static void main(String[] args) {
+        Scanner obj1 = new Scanner(System.in);
+        obj1.radix();
+        obj1.radix();
+        obj1.radix();
+
+
+    }
+}
